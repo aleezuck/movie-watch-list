b7f12115d1ed228cf31f603c11a2d8191dfde2e6
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-19 22:24:53 -0500

    Added Lists controller, policy and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'lists#index'
 
   resources :movies, only: [:index]
+  resources :lists, only: [:index, :show, :new, :create]
 end
