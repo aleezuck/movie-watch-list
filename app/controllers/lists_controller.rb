e633320fb301e2e2c57aaff05cfc5d8f3a8df177
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-20 10:23:24 -0500

    Added new action for Lists.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,26 @@ class ListsController < ApplicationController
   def index
     @lists = policy_scope(List)
   end
+
+  def new
+    @list = List.new
+    authorize(@list)
+  end
+
+  def create
+    @list = List.new(list_params)
+    authorize(@list)
+
+    if @list.save
+      redirect_to lists_path, notice: "List created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
