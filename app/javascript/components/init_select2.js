b3af77e52a700cc68552ea41eb81fa4a5e2727b6
Amanda Lee-Zuck <a.leezuck@gmail.com>
2022-01-23 17:36:53 -0500

    Moved bookmark form to Bootstrap modal.

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    dropdownParent: $('#bookmarkModal')
+  });
 };
 
 export { initSelect2 };
